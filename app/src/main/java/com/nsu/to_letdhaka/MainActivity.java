b6be234a623cfb852fd73b0d2aeaa4527d100444
sihan-tawsik <sihantawsik@gmail.com>
2019-04-02 22:14:11 +0600

    stable version

--- app/src/main/java/com/nsu/to_letdhaka/MainActivity.java
+++ app/src/main/java/com/nsu/to_letdhaka/MainActivity.java
@@ -1,16 +1,18 @@
 package com.nsu.to_letdhaka;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     }
 }
